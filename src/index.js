a8023ec68c148f585b2741e1f8acd59782500a36
kennvil102 <kennvilchez@gmail.com>
2024-06-05 15:07:39 -0600

    update4

--- src/index.js
+++ src/index.js
@@ -28,8 +28,47 @@ function agregarTarea() {
     // limpiamos el valor del texto
     document.getElementById("tareas").value = "";
 }
+ 
+//GET
+async function traerDatos() {
+    try {
+        //va esperarse y va traer a tareas.json
+        const respuesta = await fetch("tareas.json")
+        const datos = await respuesta.json()
+        datos.forEach(elemento=>{
+            let p = document.createElement("p")
+
+        })
+        console.log(datos)
+    } catch (error) {
+        console.error(error);
+    };
+}
+ // da los datos
+//POST
+async function daDatos() {
+    try {
+        let tarea = {
+            nombre:tareaTextoNueva.value,
+            estado: false
+        }
+       const respuesta = await fetch("(http://localhost:3000/api/task)",{
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              },
+              body: JSON.stringify(tarea)
+       })
+       const datos = await respuesta.json()
+       console.log(datos);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
 
+//PUT actualiza los datos
 
 
+//DELETE elimina
