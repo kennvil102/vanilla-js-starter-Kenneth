54d6dcd893ebae9e71039973c05f183280f08ef2
kennvil102 <kennvilchez@gmail.com>
2024-06-05 11:48:33 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-    // Inserte el código aquí
+function agregarTarea() {
+    // obtenemos el valor digitado
+    let tareaTextoNueva = document.getElementById("tareas").value;
+    
+    // validamos que no este vacio
+    if (tareaTextoNueva === "") {
+        alert("Escriba una tarea");
+        return;
+    }
+
+    // creamos el elemento de la lista
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = tareaTextoNueva + " ";
+
+    // creamos el  boton eliminar
+    let botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.onclick = function() {
+        nuevaTarea.remove();
+    };
+    
+    // boton de eliminar a la tarea
+    nuevaTarea.appendChild(botonEliminar);
+
+    // añadimos la tareita a la lista 
+    document.getElementById("listask").appendChild(nuevaTarea);
+
+    // limpiamos el valor del texto
+    document.getElementById("tareas").value = "";
+}
+
+
+
+
 
